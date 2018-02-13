7d0fb5dbf86f42b6fc8ca5955f6c0c6e7677b3e9
Michael Kalygin <michael.kalygin@gmail.com>
2018-02-14 02:30:39 +0300

    Fix #90: escape column name in Ranker#rearrange_ranks

--- lib/ranked-model/ranker.rb
+++ lib/ranked-model/ranker.rb
@@ -170,6 +170,7 @@ module RankedModel
 
       def rearrange_ranks
         _scope = finder
+        escaped_column = ActiveRecord::Base.connection.quote_column_name ranker.column
         unless instance.id.nil?
           # Never update ourself, shift others around us.
           _scope = _scope.where( instance_class.arel_table[instance_class.primary_key].not_eq(instance.id) )
@@ -177,15 +178,15 @@ module RankedModel
         if current_first.rank && current_first.rank > RankedModel::MIN_RANK_VALUE && rank == RankedModel::MAX_RANK_VALUE
           _scope.
             where( instance_class.arel_table[ranker.column].lteq(rank) ).
-            update_all( %Q{#{ranker.column} = #{ranker.column} - 1} )
+            update_all( %Q{#{escaped_column} = #{escaped_column} - 1} )
         elsif current_last.rank && current_last.rank < (RankedModel::MAX_RANK_VALUE - 1) && rank < current_last.rank
           _scope.
             where( instance_class.arel_table[ranker.column].gteq(rank) ).
-            update_all( %Q{#{ranker.column} = #{ranker.column} + 1} )
+            update_all( %Q{#{escaped_column} = #{escaped_column} + 1} )
         elsif current_first.rank && current_first.rank > RankedModel::MIN_RANK_VALUE && rank > current_first.rank
           _scope.
             where( instance_class.arel_table[ranker.column].lt(rank) ).
-            update_all( %Q{#{ranker.column} = #{ranker.column} - 1} )
+            update_all( %Q{#{escaped_column} = #{escaped_column} - 1} )
           rank_at( rank - 1 )
         else
           rebalance_ranks
